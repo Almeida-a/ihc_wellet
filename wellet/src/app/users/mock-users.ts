b14d9e6541b2ad760b6091756370d1c91f4c2c9d
Danielar0w0 <danielar0000@gmail.com>
2021-05-18 15:44:11 +0100

    Progress (Users)

--- wellet/src/app/users/mock-users.ts
+++ wellet/src/app/users/mock-users.ts
@@ -1,5 +1,6 @@
 import { User } from './user';
 
 export const USERS: User[] = [
-    {id: 1, name: "Alex Will", image: "https://images.unsplash.com/photo-1512485694743-9c9538b4e6e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"}
+    {id: 1, name: "Alex Will", image: "https://images.unsplash.com/photo-1512485694743-9c9538b4e6e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"},
+    {id: 2, name: "Emma Williams", image: "https://images.unsplash.com/photo-1609902726285-00668009f004?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"}
 ];

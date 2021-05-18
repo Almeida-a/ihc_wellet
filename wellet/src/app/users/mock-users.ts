3ad0b8c447f98cfdf2865f22741a9b855f20a438
Danielar0w0 <danielar0000@gmail.com>
2021-05-18 15:33:40 +0100

    Progress (Users)

---
+++ wellet/src/app/users/mock-users.ts
@@ -0,0 +1,5 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+    {id: 1, name: "Alex Will", image: "https://images.unsplash.com/photo-1512485694743-9c9538b4e6e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"}
+];
--- wellet/src/app/users/users.component.ts
+++ wellet/src/app/users/users.component.ts
@@ -1,3 +1,4 @@
+import { USERS } from './mock-users';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  users = USERS
   constructor() { }
 
   ngOnInit(): void {

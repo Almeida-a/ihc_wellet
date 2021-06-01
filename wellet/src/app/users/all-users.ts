890260e3c1ec28f5820f4e2cc24dad24b9de389e
Danielar0w0 <danielar0000@gmail.com>
2021-06-01 15:57:31 +0100

    Small improvements

--- wellet/src/app/users/all-users.ts
+++
@@ -1,7 +0,0 @@
-import { User } from './user';
-
-export const USERS: User[] = [
-    {id: 1, email: "alexwill00@live.com.pt", name: "Alex Will", description: "I'm once again asking for your financial support", image: "https://images.unsplash.com/photo-1512485694743-9c9538b4e6e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"},
-    {id: 2, email: "emmaw@ua.pt", name: "Emma Williams", description: "My one and only", image: "https://images.unsplash.com/photo-1548142813-c348350df52b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=635&q=80"},
-    {id: 3, email: "john.smith@gmail.com", name: "John Smith", description: "Hello world.", image:"https://images.unsplash.com/photo-1514415959856-84514e035ce5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"},
-];
--- wellet/src/app/users/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    id: number;
-    email: string,
-    name: string;
-    description: string;
-    image: string; //URL
-    //date: Date;
-  }

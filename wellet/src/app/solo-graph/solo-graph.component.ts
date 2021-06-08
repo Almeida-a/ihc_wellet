77c42c293bcba719ca7e2f1b8195e4cdc56d0d26
Almeida-a <aralmeida99@gmail.com>
2021-06-08 15:58:44 +0100

    Line Chart (incomplete)

--- wellet/src/app/solo-graph/solo-graph.component.ts
+++ wellet/src/app/solo-graph/solo-graph.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartType, ChartOptions } from 'chart.js';
-import { Label } from 'ng2-charts';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartType, ChartOptions, ChartDataSets } from 'chart.js';
+import { BaseChartDirective, Label } from 'ng2-charts';
+import * as pluginAnnotations from 'chartjs-plugin-annotation';
 
 @Component({
   selector: 'app-solo-graph',
@@ -10,16 +11,35 @@ import { Label } from 'ng2-charts';
 export class SoloGraphComponent implements OnInit {
 
   // Graph
-  public soloGraphOptions: ChartOptions = {
+  public soloGraphOptions: (ChartOptions & { annotation: any }) = {
     responsive: true,
-  }
-  public soloGraphLabels: Label[] = ['Expenses'];
-  public soloGraphData: number[] = [11, 12, 13];
+    annotation: {
+      annotations: [
+        {
+          type: 'line',
+          mode: 'vertical',
+          scaleID: 'x-axis-0',
+          value: 'March',
+          borderColor: 'orange',
+          borderWidth: 2,
+          label: {
+            enabled: true,
+            fontColor: 'orange',
+            content: 'LineAnno'
+          }
+        },
+      ],
+    },
+  };
+  public soloGraphLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public soloGraphData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Expenses'},
+  ];
   public soloGraphType: ChartType = 'line';
   public soloGraphLegend= true;
   public soloGraphColors = [
     {
-      backgroundColor: ["black", "orange"],
+      backgroundColor: ["grey", "orange"],
     }
   ];
 

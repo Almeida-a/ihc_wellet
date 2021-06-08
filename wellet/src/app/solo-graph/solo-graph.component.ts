a5daf246f868f64d8c4235e296508f6cc29aab05
Almeida-a <aralmeida99@gmail.com>
2021-06-08 14:32:33 +0100

    Solo graph component for displaying simple stats

--- wellet/src/app/solo-graph/solo-graph.component.ts
+++ wellet/src/app/solo-graph/solo-graph.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType, ChartOptions } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-solo-graph',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SoloGraphComponent implements OnInit {
 
+  // Graph
+  public soloGraphOptions: ChartOptions = {
+    responsive: true,
+  }
+  public soloGraphLabels: Label[] = ['Expenses'];
+  public soloGraphData: number[] = [11, 12, 13];
+  public soloGraphType: ChartType = 'line';
+  public soloGraphLegend= true;
+  public soloGraphColors = [
+    {
+      backgroundColor: ["black", "orange"],
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // events
+  public graphHovered({event, active}: {event: MouseEvent, active: {}[]}): void {
+    console.log(event, active);
   }
 
 }

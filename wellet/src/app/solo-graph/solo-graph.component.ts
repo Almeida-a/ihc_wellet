56de6f76999ba45da45367dfb98efb63fec0be63
Almeida-a <aralmeida99@gmail.com>
2021-06-13 21:44:53 +0100

    Added TODOs in analytics component and sub-components

--- wellet/src/app/solo-graph/solo-graph.component.ts
+++ wellet/src/app/solo-graph/solo-graph.component.ts
@@ -1,24 +1,40 @@
 import { Component, ViewChild } from "@angular/core";
+// Get expenses/income data
+import { expenses } from '../../storage/ExpensesStorage'
+import { incomes } from '../../storage/IncomesStorage'
+import { Transaction } from 'src/utils/Transaction';
+
 // Apex charts docs at: apexcharts.com
 import {
   ChartComponent,
-  ApexAxisChartSeries,
   ApexChart,
-  ApexXAxis,
-  ApexDataLabels,
+  ApexAxisChartSeries,
   ApexTitleSubtitle,
-  ApexStroke,
-  ApexGrid
+  ApexDataLabels,
+  ApexFill,
+  ApexYAxis,
+  ApexXAxis,
+  ApexTooltip,
+  ApexMarkers,
+  ApexAnnotations,
+  ApexStroke
 } from "ng-apexcharts";
+import { data } from "./series-data";
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
   chart: ApexChart;
-  xaxis: ApexXAxis;
   dataLabels: ApexDataLabels;
-  grid: ApexGrid;
-  stroke: ApexStroke;
+  markers: ApexMarkers;
   title: ApexTitleSubtitle;
+  fill: ApexFill;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  tooltip: ApexTooltip;
+  stroke: ApexStroke;
+  annotations: ApexAnnotations;
+  colors: any;
+  toolbar: any;
 };
 
 @Component({
@@ -27,53 +43,128 @@ export type ChartOptions = {
   styleUrls: ["./solo-graph.component.css"]
 })
 export class SoloGraphComponent {
-  @ViewChild("chart") chart?: ChartComponent;
+  @ViewChild("chart", { static: false }) chart: ChartComponent;
   public chartOptions: Partial<ChartOptions>;
+  public activeOptionButton = "all";
+  public updateOptionsData = {
+    "1m": {
+      xaxis: {
+        min: new Date("28 Jan 2013").getTime(),
+        max: new Date("27 Feb 2013").getTime()
+      }
+    },
+    "6m": {
+      xaxis: {
+        min: new Date("27 Sep 2012").getTime(),
+        max: new Date("27 Feb 2013").getTime()
+      }
+    },
+    "1y": {
+      xaxis: {
+        min: new Date("27 Feb 2012").getTime(),
+        max: new Date("27 Feb 2013").getTime()
+      }
+    },
+    "1yd": {
+      xaxis: {
+        min: new Date("01 Jan 2013").getTime(),
+        max: new Date("27 Feb 2013").getTime()
+      }
+    },
+    all: {
+      xaxis: {
+        min: undefined,
+        max: undefined
+      }
+    }
+  };
 
   constructor() {
+    this.initChart();
+  }
+
+  initChart(): void {
     this.chartOptions = {
       series: [
         {
-          name: "Food",
-          data: [350, 200, 350, 510, 490, 620, 300, 500, 148]
+          data: data
         }
       ],
       chart: {
-        height: 350,
-        type: "line",
-        zoom: {
-          enabled: false
-        }
+        type: "area",
+        height: 350
+      },
+      annotations: {
+        yaxis: [
+          {
+            y: 30,
+            borderColor: "#999",
+            label: {
+              text: "Support",
+              style: {
+                color: "#fff",
+                background: "#00E396"
+              }
+            }
+          }
+        ],
+        xaxis: [
+          {
+            x: new Date("14 Nov 2012").getTime(),
+            borderColor: "#999",
+            label: {
+              text: "Rally",
+              style: {
+                color: "#fff",
+                background: "#775DD0"
+              }
+            }
+          }
+        ]
       },
       dataLabels: {
         enabled: false
       },
-      stroke: {
-        curve: "smooth"
-      },
-      title: {
-        text: "Expenses: monthly",
-        align: "left"
-      },
-      grid: {
-        row: {
-          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-          opacity: 0.5
-        }
+      markers: {
+        size: 0
       },
       xaxis: {
-        categories: [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep"
-        ]
+        type: "datetime",
+        min: new Date("01 Mar 2012").getTime(),
+        tickAmount: 6
+      },
+      tooltip: {
+        x: {
+          format: "dd MMM yyyy"
+        }
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shadeIntensity: 1,
+          opacityFrom: 0.7,
+          opacityTo: 0.9,
+          stops: [0, 100]
+        }
       }
     };
   }
+
+  public updateOptions(option: any): void {
+    this.activeOptionButton = option;
+    this.chart.updateOptions(this.updateOptionsData[option], false, true, true);
+    // continue here... [refactoring solo-graph]
+  }
+
+  public getExpensesData = function(category: string) : number[] {
+    
+    // Get all (expenses) transactions 
+    // ...
+    // Filter transactions where t.category()==category
+    // ...
+    // Get their dates and values
+
+    return [-1];
+  }
+
 }
